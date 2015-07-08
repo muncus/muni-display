ab0e457bd8d9e8905cd5b7346a44cae2c08daf5e
Marc Dougherty <muncus@gmail.com>
2015-07-08 15:49:48 -0700

    delete commented-out code, fix flush() problems.
    There are still bugs in the http response parsing that need hunting..

--- device/gauge.ino
+++ device/gauge.ino
@@ -24,17 +24,6 @@ void setup(){
   Spark.subscribe("spark/device/name", name_handler);
   Spark.publish("spark/device/name");
 
-  /*
-  gauge.attach(D0);
-  gauge.write(0);
-  delay(500);
-  gauge.detach();
-  */
-
-  //Spark.function("gauge", setGaugeAngle);
-  //Spark.function("times", setTimes);
-  //Spark.variable("angle", &set_angle, INT);
-
 }
 
 // Callback to set the device name, from the cloud.
@@ -46,63 +35,38 @@ void name_handler(const char *topic, const char *data) {
 
 void loop(){
 
-  // Show times by converting to angle.
-  // loop backwards, pausing to allow for visibility of all known times.
-  /*
-  for(int i=MAXTIMES-1;i>=0;i--){
-    int angle = map(known_times[i], 0, 30, 0, 180);
-    constrain(angle, 0, 180);
-    setGaugeAngle(angle);
-    delay (500);
-  }
-  */
-  //Serial.println(my_device_name);
-  /*
-  delay(2000);
-  Serial.println("ANGLES");
-  for( int i=0;i <= 180;i+=10){
-    gauge.attach(D0);
-    Serial.println("setting: " + String(i));
-    gauge.write(i);
-    delay(700);
-    gauge.detach();
-    delay(1000);
-  }
-  */
   if(millis() > last_fetch + (fetch_interval_s * 1000) && !client.connected()){
     last_fetch = millis();
     Serial.println("attempting to get times...");
-    Serial.println("Got: " + getTimes());
+    int t = getTime();
+    if(t<0){
+      Serial.println("An error occurred while fetching times.");
+    } else {
+      Serial.println("Got: " + String(t));
+      int angle = map(t, 0, 30, 0, 180);
+      Serial.println("angle: " + String(angle));
+      constrain(angle, 0, 180);
+      setGaugeAngle(angle);
+    }
   }
 }
 
-int setGaugeAngle(String angle) {
-  int requested_angle = angle.toInt();
+int setGaugeAngle(int angle) {
+  gauge.attach(D0);
+  int requested_angle = angle;
   if(requested_angle > 180 || requested_angle < 0){
     return -1;
   }
-  set_angle = requested_angle;
+  //set_angle = requested_angle;
   gauge.write(requested_angle);
+  delay(700);
+  gauge.detach();
   return 1;
 };
 
-// Assume times are comma-separated.
-int setTimes(String times) {
-  int startpos = 0;
-  for(int i=0;i<MAXTIMES;i++){
-    int limit = (times.indexOf(',', startpos) || times.length()-1);
-    // substring is not inclusive.
-    int t = times.substring(startpos, limit).toInt();
-    Serial.println("Found time:" + String(t));
-    known_times[i] = t;
-    startpos = limit+1;
-  }
-  return 1;
-}
 
 // Fetch the list of times, and return it.
-String getTimes(){
-  char data[50];
+int getTime(){
   //146.148.61.247
   IPAddress svr(146,148,61,247);
   //byte svr[] = { 146, 148, 61, 247 };
@@ -124,49 +88,42 @@ String getTimes(){
     delay(300);
     int i = 0;
     char buffer[100];
-    //String datastring;
-    //datastring.reserve(50);
-    char data[50];
+    char data_buffer[50];
 
     while(client.available() && i<100){
       buffer[i] = client.read();
       //Serial.print(String(buffer[i]));
       if(buffer[i] == '\n'){
         //line end, check for empty.
-        Serial.println("NEWLINE");
+        //Serial.println("NEWLINE");
         // 13 == \r
         //Serial.println("Previous char: " + String(buffer[i-1], DEC));
-        if(i < 4){
+        if(i < 3){
           //empty line.
-          Serial.println("FOUND EMPTY");
+          //Serial.println("FOUND EMPTY");
           int j = 0;
           while(client.available() && j<50){
             char c = client.read();
-            data[j] = c;
-            //datastring.setCharAt(j, c);
+            Serial.println(c);
+            data_buffer[j] = c;
             j++;
           }
-          Serial.println("DATA: " + String(data));
-          //Serial.println("DATASTRING; " + datastring);
-          return String(data);
+          //Serial.println("DATA: " + String(data_buffer));
+          client.flush();
+          client.stop();
+          return String(data_buffer).toInt();
         }
-        //Serial.println("RESET");
-        i = 0; //reset for next line.
+        else {
+          i = 0; //reset for next line.
+        }
       }
       i++;
-      if (i >= 100){
-        Serial.println("buffer full.");
-        i = 0;
-      }
     }
-    //client.flush();
-    client.stop();
   }
   else {
     Serial.println("Connection failed!");
-    return String("");
   }
-  //client.flush();
-  //client.stop();
-  //return String("");
+  client.flush();
+  client.stop();
+  return -1;
 }
